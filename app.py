4d0facee4bd3a5158aac44ef1ae0b6ee9a599de3
Tim <c00456210@louisiana.edu>
2020-04-21 01:56:03 -0400

    CRUD works

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask, jsonify, request
-from  queries import TaskQuery
+from flask import Flask
+import flask_sqlalchemy
+import flask_restless
+from sqlalchemy import Column, String, Integer, Sequence
 
 app = Flask(__name__)
 
-@app.route('/tasks')
-def get_all_tasks():
-    return jsonify(TaskQuery.get_all())
+app.config.from_pyfile('config.py')
 
+db = flask_sqlalchemy.SQLAlchemy(app)
+
+class Task(db.Model):
+    __tablename__ = 'task'
+    taskno = db.Column(Integer, Sequence('task_id_seq'), primary_key=True)
+    description = db.Column(String)
+
+manager = flask_restless.APIManager(app, flask_sqlalchemy_db=db)
+manager.create_api(Task, methods=['GET', 'POST', 'PATCH', 'DELETE'])
 
---
+++ config.py
@@ -0,0 +1,5 @@
+USER = 'system'
+PASSWORD = 'oracle'
+TNSNAME = 'orcl'
+SQLALCHEMY_DATABASE_URI = f'oracle+cx_oracle://{USER}:{PASSWORD}@{TNSNAME}'
+
